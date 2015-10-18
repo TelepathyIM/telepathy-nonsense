3ad2b9b1fb847c76446323492c6d470ee2840334
Niels Ole Salscheider <niels_ole@salscheider-online.de>
2015-10-18 12:42:07 +0200

    Add licence information

--- uniquehandlemap.hh
+++ uniquehandlemap.hh
@@ -1,15 +1,20 @@
 /*
-    Copyright (C) 2015 Niels Ole Salscheider <niels_ole@salscheider-online.de>
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-    LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-    OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-    WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-*/
+ * This file is part of the telepathy-nonsense connection manager.
+ * Copyright (C) 2015 Niels Ole Salscheider <niels_ole@salscheider-online.de>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>
+ */
 
 #ifndef UNIQUEHANDLEMAP_HH
 #define UNIQUEHANDLEMAP_HH
